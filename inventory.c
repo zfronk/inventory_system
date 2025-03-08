7df542fb0196fd47a5826fc7557bf07884cb8767
zfronk <officialstevve@gmail.com>
2025-03-08 14:08:47 +0300

    second_commit

--- inventory.c
+++ inventory.c
@@ -3,15 +3,125 @@
 #include <string.h>
 #include <stdlib.h>
 
+// WAREHOUSE INVENTORY SYSTEM
+
+// Initialize struct to item
+struct ITEM{
+    char name[100];
+    int price;
+
+};
+
+
 // Add item 
 void add_item(){
-    printf("\nYou're adding an item");
-    printf("\n");
+    
+    // Address to struct where item name, price is stored
+    struct ITEM *item = (struct ITEM*)malloc(sizeof(struct ITEM));  // -- > NO segmentation faults 
+
+    if(item == NULL){
+        printf("\nMemory allocation failed");
+        return;
+
+    }
+    
+    
+    // printf("\nAdding item name...");
+    // printf("\n");
+    
+
+    // ITEM NAME
+    
+    bool prompt_item_name = true;
+    
+    // Keep checking for item - name
+    while(prompt_item_name){
+        printf("\nEnter item name: ");
+        
+        // Leaves a newline
+        if(fgets(item -> name, sizeof(item -> name), stdin) == NULL){
+            printf("\nError occured! Exited function.");
+            prompt_item_name = false;
+
+        }
+
+        // If enter
+        else if(strcmp(item -> name, "\n") == 0){
+            printf("You pressed enter, input is empty!");
+            printf("\n");
+
+        }
+
+        else{
+            // Remove new line character
+            item -> name[strcspn(item -> name, "\n")] = 0;
+            printf("Item added!");
+            printf("\n");
+            //printf("\nInput: %s", item -> name);
+            prompt_item_name = false;   
+        }
+
+        
+
+    }
+
+
+    // printf("\nAdding item price...");
+    // printf("\n");
+
+    // ITEM PRICE
+    bool prompt_item_price = true;
+    
+
+
+    while(prompt_item_price){
+        printf("\nEnter item price: ");
+
+        // Pick the first char compare
+        char firstchar = getchar();
+
+        // Check for Enter
+        if(firstchar == '\n'){
+            printf("You pressed enter! Try again");
+            printf("\n");
+            continue;
+
+        }
+
+        // If not put the char back to stdin
+        ungetc(firstchar, stdin);
+
+        
+
+        // Try again if not integer
+        if(scanf("%d", &item -> price) != 1){
+            printf("Inappropriate data type! Use integers");
+            while(getchar() != '\n'); // Flush remaining buffer remove bugs
+            printf("\n");
+            continue;
+        }
+
+        // scanf leaves behind "\n" when user presses enter! // This leaves bugs
+
+        else{
+            while(getchar() != '\n'); // Flush remaining buffer remove bugs
+            printf("Price added!");
+            printf("\n");
+
+            prompt_item_price = false;
+
+        }
+
+        
+
+    }
+
+
 
 }
 
 // View list of items
-void view_item_list(){
+void display_item_list(){
     printf("\nYou're viewing the item list");
     printf("\n");
     
@@ -19,7 +129,8 @@ void view_item_list(){
 
 // Invalid option
 void invalid_option(){
-    printf("\nInvalid option");
+    printf("Invalid option");
+    printf("\n");
     return;
 }
 
@@ -29,9 +140,13 @@ void invalid_option(){
 void manage_dashboard(){
     
     // Dashboard
-    printf("\nWelcome to the inventory system");
+    printf("\nWAREHOUSE INVENTORY MANAGEMENT SYSTEM");
+    printf("\n=====================================");
 
     printf("\n");
+    
+    printf("\nDASHBOARD");
+    printf("\n=========");
     printf("\n1.Add item");
     printf("\n2.View item list");
     printf("\n3.Close application\n");
@@ -42,7 +157,7 @@ void manage_dashboard(){
 
 
     while(is_running){
-        printf("\nChoose an option: ");
+        printf("\nChoose an option from the dashboard: ");
 
         // Check for empty input (Enter key) -- Takes one line and reads
         int first_char = getchar();
@@ -73,14 +188,15 @@ void manage_dashboard(){
 
         else if(choice == 2){
             getchar();
-            view_item_list();
+            display_item_list();
             continue;
 
         }
 
         else if(choice == 3){
             getchar();
-            printf("\nBye!\n");
+            printf("\nExited");
+            printf("\n\n");
             is_running = false;
 
 
